8b603e932815589ba0053fd21256bb66de1faf93
thermius <156609541+thermius@users.noreply.github.com>
2026-08-28 19:20:53 -0300

    Add files via upload

---
+++ lib_padrao/lib.c
@@ -0,0 +1,10 @@
+#include "lib.h"
+
+
+/*Função que inverte o numero passado*/
+int LibFuncaoUnario (int valor)
+{
+	return -valor;
+}
+
+
---
+++ lib_padrao/lib.h
@@ -0,0 +1,6 @@
+#ifndef LIB_PADRAO_H
+#define LIB_PADRAO_H
+
+int LibFuncaoUnario (int);
+
+#endif
